fac0c7abbb933e50008e75b6fca9815ed0f36947
0wlie22 <dari.sedova@gmail.com>
2023-01-20 22:54:20 +0200

    Continue working on check-sequence.py

--- arrays/check-sequence.py
+++ arrays/check-sequence.py
@@ -1,7 +1,31 @@
-def if_first_come_first_served():
-    ...
+in_orders: list = [2, 4, 6]
+out_orders: list = [1, 3, 5]
+served_orders: list = [1, 2, 4, 6, 3, 5]
+
+
+def if_first_come_first_served(out_orders: list, in_orders: list, servings: list) -> bool:
+    in_index: int = 0
+    out_index: int = 0
+    for order in servings:
+        if in_index < len(in_orders) - 1:
+            if order == in_orders[in_index]:
+                in_index += 1
+        else:
+            in_index += 1
+
+        if out_index < len(out_orders) - 1:
+            if order == out_orders[out_index]:
+                out_index += 1
+        else:
+            out_index += 1
+    print(in_index, out_index)
+    if in_index == len(in_orders) and out_index == len(out_orders):
+        return True
+    return False
 
 
 if __name__ == '__main__':
-    if_first_come_first_served()
-    
+    if if_first_come_first_served(out_orders, in_orders, served_orders):
+        print("true")
+    else:
+        print("false")
